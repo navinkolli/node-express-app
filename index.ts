b666b5d35ff66e49455b9ef96764886e8d604082
navin <naviinkolli@gmail.com>
2019-06-25 13:29:43 -0400

    api-tst

--- index.ts
+++ index.ts
@@ -1,30 +1,45 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({
-  logger: true
-})
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+const express = require('express');
+const app = express();
 
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start();
+// // Require the framework and instantiate it
+// const fastify = require('fastify')({
+//   logger: true
+// })
 
-// Require external modules
-const mongoose = require('mongoose')
-// Connect to DB
-mongoose.connect("mongodb://localhost/node-db")
- .then((e) => {
-console.log("MongoDB connected…",e)
- } )
- .catch(err => console.log(err))
+// // Declare a route
+// fastify.get('/', async (request, reply) => {
+//   return { hello: 'world' }
+// })
+
+// // Run the server!
+// const start = async () => {
+//   try {
+//     await fastify.listen(3000)
+//     fastify.log.info(`server listening on ${fastify.server.address().port}`)
+//   } catch (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+// }
+// start();
+
+// // // Require external modules
+// // const mongoose = require('mongoose')
+// // // Connect to DB
+// // mongoose.connect("mongodb://localhost/node-db")
+// //  .then((e) => {
+// // console.log("MongoDB connected…")
+// //  } )
+// //  .catch(err => console.log(err))
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.get('/testing', function (req, res, next) {
+ res.json(["Kuasa","Ravi","Abhi","Cleveland","test"]);
+});
+app.listen(3000, function () {
+  console.log('Node App listening on port 3000!');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.get('/url', function (req, res, next) {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
-app.listen(3000, function () {
-  console.log('Node App listening on port 3000!');
-});
+// const express = require('express');
+// const app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.get('/url', function (req, res, next) {
+//  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+// });
+// app.listen(3000, function () {
+//   console.log('Node App listening on port 3000!');
+// });
